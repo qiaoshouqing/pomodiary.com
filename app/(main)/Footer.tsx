c46cf9dc224d16cac95741c64bfe99a4b591d2bd
qiaoshouqing <1143878969@qq.com>
2024-07-01 22:12:44 +0800

    调整品牌

--- app/(main)/Footer.tsx
+++ app/(main)/Footer.tsx
@@ -13,7 +13,7 @@ import { env } from '~/env.mjs'
 import { prettifyNumber } from '~/lib/math'
 import { redis } from '~/lib/redis'
 
-import { Newsletter } from './Newsletter'
+// import { Newsletter } from './Newsletter'
 
 function NavLink({
   href,
